2c0db5f52a8484263a07008fad717bc586647ca3
Felix Kimutai <kmtflx@gmail.com>
2025-06-08 13:43:59 +0300

    async and await function added

--- script.js
+++ script.js
@@ -426,3 +426,19 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 );
 
 booksAfterUpdate;
+
+// Asynchronous JavaScript: Promise
+
+/*
+fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+  res.json().then((data) => console.log(data))
+);
+*/
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+getTodos();
